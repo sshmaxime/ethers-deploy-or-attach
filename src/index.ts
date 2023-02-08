cf8e9045c2e4921c3453387ba2189a9d9f4c14a7
Maxime Aubanel <maximeauba@gmail.com>
2023-02-08 03:18:35 +0100

    First iteration

---
+++ src/index.ts
@@ -0,0 +1,80 @@
+import { Signer, ContractFactory } from "ethers";
+
+type AsyncReturnType<T extends (...args: any) => any> = T extends (...args: any) => Promise<infer U>
+  ? U
+  : T extends (...args: any) => infer U
+  ? U
+  : any;
+
+export type Contract<F extends ContractFactory> = AsyncReturnType<F["deploy"]>;
+
+type inputContracts = {
+  [contractName: string]:
+    | ReturnType<ReturnType<typeof initDeployOrAttach>["deployOrAttach"]>
+    | ReturnType<ReturnType<typeof initDeployOrAttach>["attach"]>;
+};
+
+export type outputContracts = inputContracts & {
+  connect: (signer: Signer) => outputContracts;
+};
+
+export interface ContractBuilder<F extends ContractFactory> {
+  metadata: {
+    contractName: string;
+    abi: any;
+    bytecode: string;
+  };
+  deploy(...args: Parameters<F["deploy"]>): Promise<Contract<F>>;
+  attach(address: string, signer?: Signer): Promise<Contract<F>>;
+}
+
+export type FactoryConstructor<F extends ContractFactory> = {
+  new (signer?: Signer): F;
+  abi: any;
+  bytecode: string;
+};
+
+export const buildContracts = <F extends { (signer?: Signer): inputContracts }>(funct: F) => {
+  return {
+    connect: (signer: Signer) => funct(signer),
+    ...funct(),
+  } as ReturnType<F> & outputContracts;
+};
+
+export const initDeployOrAttach = (ethers: { getSigners(): Promise<any[]> }) => {
+  const attach = <F extends ContractFactory>(
+    FactoryConstructor: FactoryConstructor<F>,
+    initialSigner?: Signer
+  ) => {
+    return {
+      attach: async (address: string, signer?: Signer): Promise<Contract<F>> => {
+        const defaultSigner = initialSigner || (await ethers.getSigners())[0];
+        return new FactoryConstructor(signer || defaultSigner).attach(address) as Contract<F>;
+      },
+    };
+  };
+
+  const deployOrAttach = <F extends ContractFactory>(
+    FactoryConstructor: FactoryConstructor<F>,
+    initialSigner?: Signer
+  ): ContractBuilder<F> => {
+    const contractName = FactoryConstructor.name.replace("__factory", "");
+    return {
+      metadata: {
+        contractName: contractName,
+        abi: FactoryConstructor.abi,
+        bytecode: FactoryConstructor.bytecode,
+      },
+      deploy: async (...args: Parameters<F["deploy"]>): Promise<Contract<F>> => {
+        const defaultSigner = initialSigner || (await ethers.getSigners())[0];
+        return new FactoryConstructor(defaultSigner).deploy(...(args || [])) as Contract<F>;
+      },
+      attach: attach<F>(FactoryConstructor, initialSigner).attach,
+    };
+  };
+
+  return {
+    deployOrAttach,
+    attach,
+  };
+};
